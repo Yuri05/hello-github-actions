44c0901c91625dd67281a9986fc986366df09b4b
Yuri05 <Yuri05@github.com>
2021-06-02 13:29:37 +0200

    3.6-Centos7

--- .github/workflows/install_packages.R
+++ .github/workflows/install_packages.R
@@ -13,17 +13,4 @@ install.packages('gridExtra', repos='http://cran.rstudio.com/')
 install.packages('reshape2', repos='http://cran.rstudio.com/')
 install.packages('ggplot2', repos='http://cran.rstudio.com/')
 install.packages('testthat', repos='http://cran.rstudio.com/')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+install.packages('devtools', repos='http://cran.rstudio.com/')
